73a8504e55fe20232d92ed5558c79ed2a49eb698
Dzmitry <gera.1212@yandex.ru>
2023-06-21 20:47:07 +0300

    add edits

--- source/js/script.js
+++ source/js/script.js
@@ -1,1 +1,14 @@
-/* в этот файл добавляет скрипты*/
+const navMain = document.querySelector('.main-nav');
+const navToggle = document.querySelector('.main-nav__toggle');
+const userList = document.querySelector('.user-list');
+
+navMain.classList.remove('main-nav--nojs');
+userList.classList.remove('user-list--nojs');
+
+navToggle.addEventListener('click', function () {
+  if (navMain.classList.contains('main-nav--closed')) {
+    navMain.classList.replace('main-nav--closed', 'main-nav--opened');
+  } else {
+    navMain.classList.replace('main-nav--opened', 'main-nav--closed');
+  }
+});
