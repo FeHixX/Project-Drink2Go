aa67bcaf5b2a077882b27875b372fe22f2232168
Dzmitry <gera.1212@yandex.ru>
2023-06-25 20:41:27 +0300

    add map all permissions

--- source/js/script.js
+++ source/js/script.js
@@ -2,6 +2,10 @@ const navMain = document.querySelector('.main-nav');
 const navToggle = document.querySelector('.main-nav__toggle');
 const userList = document.querySelector('.user-list');
 
+const mapImageTag = document.querySelector('.map__wrapper > picture');
+const mapGoogle = document.querySelector('.map__google');
+
+// mobile menu
 navMain.classList.remove('main-nav--nojs');
 userList.classList.remove('user-list--nojs');
 
@@ -12,3 +16,7 @@ navToggle.addEventListener('click', function () {
     navMain.classList.replace('main-nav--opened', 'main-nav--closed');
   }
 });
+
+// map
+mapImageTag.remove();
+mapGoogle.classList.remove('map__google--nojs');
