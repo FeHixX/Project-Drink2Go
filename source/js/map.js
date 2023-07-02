aea74360b098078c4fff68d63e524a3831d29494
Dzmitry <gera.1212@yandex.ru>
2023-07-02 13:55:48 +0300

    add leaflet map

--- source/js/map.js
+++ source/js/map.js
@@ -1,45 +1,20 @@
-const yandexMapScript = document.createElement('script');
-yandexMapScript.src = 'https://api-maps.yandex.ru/2.1/?apikey=&lang=ru_RU';
+document.querySelector("#map").querySelector("picture").remove();
 
-yandexMapScript.onload = () => {
-  document.querySelector('#map').querySelector('picture').remove();
+const coordinate = [59.96845, 30.3174];
+const markerText = `
+<p>г. Санкт-Петербург, набережная реки Карповки, дом 5</p>
+<p>Номер телефона: <a href="tel:+79999999999">+7 (999) 999-99-99</a></p>
+`;
 
-  const CENTER = [59.9685, 30.3174];
+const map = L.map('map').setView(coordinate, 18);
 
-  ymaps.ready(() => {
-    const map = new ymaps.Map('map', {
-      center: CENTER,
-      zoom: 17.68,
-      controls: [],
-    });
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
 
-    const placemark = new ymaps.Placemark(
-      CENTER,
-      {
-        balloonContentHeader: 'Адрес',
-        balloonContentBody: 'г. Санкт-Петербург, набережная реки Карповки, дом 5',
-        balloonContentOffset: [-4, -25]
-      },
-      {
-        iconLayout: 'default#image',
-        iconImageHref: '../img/icons/stack.svg#icon-map-pin',
-        iconImageSize: [38, 50],
-        iconImageOffset: [-19, -50]
-      }
-    );
+const icon = L.icon({
+  iconUrl: '../img/icons/stack.svg#icon-map-pin',
+  iconSize: [38, 50],
+  iconAnchor: [4, 25],
+  popupAnchor: [15, 25]
+});
 
-    map.geoObjects.add(placemark);
-  });
-};
-
-console.log(scrollY);
-const scrollWindowHanlder = () => {
-  console.log(scrollY)
-  if (window.scrollY > 1000) {
-    document.head.append(yandexMapScript);
-
-    document.removeEventListener('scroll', scrollWindowHanlder);
-  }
-};
-
-document.addEventListener('scroll', scrollWindowHanlder);
+L.marker(coordinate, { icon }).addTo(map).bindPopup(markerText);
